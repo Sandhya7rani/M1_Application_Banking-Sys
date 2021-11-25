c21a849fa099db5838d38c8b464805cdc9d6c562
Sandhya7rani <57440397+Sandhya7rani@users.noreply.github.com>
2021-11-25 09:01:33 +0530

    Update BankOperations.c

--- 3_Implementation/src/BankOperations.c
+++ 3_Implementation/src/BankOperations.c
@@ -1,14 +1,15 @@
 #include "../inc/BankOperations.h"
+#include "../inc/Account_details.h"
 #include<string.h>
 #include<stdlib.h>
 #include<stdio.h>
-int login(int accountnumber,int password)
+int login(details* Details)
 {
-    if((accountnumber==12345678 && password==11111111)||
-    (accountnumber==23456789 && password==22222222)||
-    (accountnumber==34567890 && password==333333333)||
-    (accountnumber==10101010 && password==44444444)||
-    (accountnumber==20202020 && password==55555555))
+    if((Details->accountnumber==12345678 && Details->password==11111111)||
+    (Details->accountnumber==23456789 && Details->password==22222222)||
+    (Details->accountnumber==34567890 && Details->password==333333333)||
+    (Details->accountnumber==10101010 && Details->password==44444444)||
+    (Details->accountnumber==20202020 && Details->password==55555555))
     return 0;
     else
     return -9;
